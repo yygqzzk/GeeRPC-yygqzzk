515e4460884d6d374e4ee81800e05004a73224e7
yygqzzk <1374174364@qq.com>
2025-06-28 02:42:03 +0800

    feat: 支持负载均衡功能

---
+++ xclient/discovery.go
@@ -0,0 +1,89 @@
+package xclient
+
+import (
+	"errors"
+	"math"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+type selectMode int
+
+const (
+	RandomSelect selectMode = iota
+	RoundRobinSelect
+)
+
+// 定义服务发现接口
+type Discovery interface {
+	Refresh() error
+	Update(servers []string) error
+	Get(mode selectMode) (string, error)
+	GetAll() ([]string, error)
+}
+
+// 服务发现结构体
+type MultiServersDiscovery struct {
+	r       *rand.Rand   // 生成随机数
+	mu      sync.RWMutex // 读写锁
+	servers []string
+	index   int // 记录轮旋算法时的轮询下标
+}
+
+func NewMultiServerDiscovery(servers []string) *MultiServersDiscovery {
+	d := &MultiServersDiscovery{
+		servers: servers,
+		r:       rand.New(rand.NewSource(time.Now().UnixNano())),
+	}
+	d.index = d.r.Intn(math.MaxInt32 - 1)
+	return d
+}
+
+var _ Discovery = (*MultiServersDiscovery)(nil)
+
+// TODO
+func (d *MultiServersDiscovery) Refresh() error {
+	return nil
+}
+
+// 动态刷新服务列表
+func (d *MultiServersDiscovery) Update(servers []string) error {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	d.servers = servers
+	return nil
+}
+
+// 根据负载均衡算法获取服务实例
+func (d *MultiServersDiscovery) Get(mode selectMode) (string, error) {
+	d.mu.RLock()
+	defer d.mu.RUnlock()
+
+	n := len(d.servers)
+	if n == 0 {
+		return "", errors.New("rpc discovery: no available servers")
+	}
+
+	switch mode {
+	case RandomSelect:
+		return d.servers[d.r.Intn(n)], nil
+	case RoundRobinSelect:
+		s := d.servers[d.index%n]
+		d.index = (d.index + 1) % n
+		return s, nil
+	default:
+		return "", errors.New("rpc discovery: not supported select mode")
+	}
+}
+
+// 获取所有服务
+func (d *MultiServersDiscovery) GetAll() ([]string, error) {
+	d.mu.RLock()
+	defer d.mu.RUnlock()
+	servers := make([]string, 0, len(d.servers))
+
+	copy(servers, d.servers)
+
+	return servers, nil
+}
---
+++ xclient/xclient.go
@@ -0,0 +1,116 @@
+package xclient
+
+import (
+	"context"
+	"geeRPC"
+	"io"
+	"reflect"
+	"sync"
+)
+
+type XClient struct {
+	d       Discovery
+	mode    selectMode
+	opt     *geeRPC.Option
+	mu      sync.Mutex
+	clients map[string]*geeRPC.Client
+}
+
+var _ io.Closer = (*XClient)(nil)
+
+func (xc *XClient) Close() error {
+	xc.mu.Lock()
+	defer xc.mu.Unlock()
+
+	for key, value := range xc.clients {
+		_ = value.Close()
+		delete(xc.clients, key)
+	}
+
+	return nil
+}
+
+func NewXClient(d Discovery, mode selectMode, opt *geeRPC.Option) *XClient {
+	return &XClient{
+		d:       d,
+		mode:    mode,
+		opt:     opt,
+		clients: make(map[string]*geeRPC.Client),
+	}
+}
+
+func (xc *XClient) dial(rpcAddr string) (*geeRPC.Client, error) {
+	xc.mu.Lock()
+	defer xc.mu.Unlock()
+	client, ok := xc.clients[rpcAddr]
+	// 存在缓存client，但不可用
+	if ok && !client.IsAvailable() {
+		_ = client.Close()
+		delete(xc.clients, rpcAddr)
+		client = nil
+	}
+	// 不存在缓存client
+	if client == nil {
+		var err error
+		client, err = geeRPC.XDial(rpcAddr, xc.opt)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return client, nil
+}
+
+func (xc *XClient) call(rpcAddr string, ctx context.Context, serviceMethod string, args, reply interface{}) error {
+	client, err := xc.dial(rpcAddr)
+	if err != nil {
+		return err
+	}
+	return client.Call(ctx, serviceMethod, args, reply)
+}
+
+func (xc *XClient) Call(ctx context.Context, serviceMethod string, args, reply interface{}) error {
+	rpcAddr, err := xc.d.Get(xc.mode)
+	if err != nil {
+		return err
+	}
+
+	return xc.call(rpcAddr, ctx, serviceMethod, args, reply)
+}
+
+func (xc *XClient) Broadcast(ctx context.Context, serviceMethod string, args, reply interface{}) error {
+	servers, err := xc.d.GetAll()
+	if err != nil {
+		return err
+	}
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+	var e error
+	// 是否已经填充 reply（nil 表示不需要返回值）
+	replyDone := reply == nil
+	// 创建可取消的上下文，优雅终止其它 goroutine
+	ctx, cancel := context.WithCancel(ctx)
+
+	for _, rpcAddr := range servers {
+		wg.Add(1)
+		go func(rpcAddr string) {
+			defer wg.Done()
+			var clonedReply interface{}
+			if reply != nil {
+				clonedReply = reflect.New(reflect.ValueOf(reply).Elem().Type()).Interface()
+			}
+			err := xc.call(rpcAddr, ctx, serviceMethod, args, clonedReply)
+			mu.Lock()
+			if err != nil && e == nil {
+				e = err
+				cancel()
+			}
+			if err == nil && !replyDone {
+				reflect.ValueOf(reply).Elem().Set(reflect.ValueOf(clonedReply).Elem())
+				replyDone = true
+			}
+		}(rpcAddr)
+
+	}
+	wg.Wait()
+	return e
+}
